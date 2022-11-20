514ff6b101ae17f3dfdd77cfe34376d9c6d3acc6
Karen Costa <karen.pcosta@gmail.com>
2022-11-20 00:38:45 -0300

    Add code to renderFeed function to create comment html template and define its position. Create function to toggle its display status

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.addEventListener("click", function (e) {
     likeButton(e.target.dataset.like);
   } else if (e.target.dataset.retweet) {
     retweetButton(e.target.dataset.retweet);
+  } else if (e.target.dataset.comment) {
+    commentButton(e.target.dataset.comment);
   }
 });
 
@@ -36,36 +38,60 @@ function retweetButton(uuid) {
   renderFeed();
 }
 
+function commentButton(uuid) {
+  document.getElementById(`replies-${uuid}`).classList.toggle("hidden");
+}
+
 function renderFeed() {
   let feedHtml = "";
   for (let tweet of tweetsData) {
     const hasBeenLiked = tweet.isLiked ? "liked" : "";
     const hasBeenRetweeted = tweet.isRetweeted ? "retweeted" : "";
 
-    feedHtml += `
-        <div class="tweet">
-    <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                <i class="fa-regular fa-comment" data-comment="${tweet.uuid}"></i>
-                ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                <i class="fa-sharp fa-solid fa-heart ${hasBeenLiked}" data-like="${tweet.uuid}"></i>
-                ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                <i class="fa-solid fa-retweet ${hasBeenRetweeted}" data-retweet="${tweet.uuid}"></i>
-                ${tweet.retweets} 
-                </span>
-            </div>   
-        </div>            
+    let commentHtml = "";
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (comment) {
+        commentHtml += `<div class="tweet-reply">
+        <div class="tweet-inner">
+            <img src="${comment.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${comment.handle}</p>
+                    <p class="tweet-text">${comment.tweetText}</p>
+                </div>
+            </div>
     </div>
-</div> `;
+    `;
+      });
+    }
+
+    feedHtml += `
+    <div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                    <i class="fa-regular fa-comment" data-comment="${tweet.uuid}"></i>
+                    ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                    <i class="fa-sharp fa-solid fa-heart ${hasBeenLiked}" data-like="${tweet.uuid}"></i>
+                    ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${hasBeenRetweeted}" data-retweet="${tweet.uuid}"></i>
+                    ${tweet.retweets} 
+                    </span>
+                </div>   
+            </div>            
+        </div>
+        <div class="hidden" id="replies-${tweet.uuid}"> 
+        ${commentHtml}
+        </div>
+    </div> `;
   }
   document.getElementById("feed").innerHTML = feedHtml;
 }
